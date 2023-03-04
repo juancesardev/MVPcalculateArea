09cf16540ff096b90856eae5df46371314b2544d
Juan Cesar <juancesardev@gmail.com>
2023-03-04 11:13:41 -0400

    up

---
+++ MVPtest1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using MVPtest1.Presenters;
+using MVPtest1.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVPtest1
+{
+    public partial class Form1 : Form, IRectangle
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string LengthText 
+        {
+            get { return txtLength.Text; }
+            set { txtLength.Text = value; }
+        }
+        public string BreadthText 
+        {
+            get { return txtBreadth.Text;}
+            set { txtBreadth.Text = value;}
+        }
+        public string AreaText 
+        {
+            get { return lblArea.Text; }
+            set
+            {
+                lblArea.Text = value;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RectanglePresenter presenter = new RectanglePresenter(this);
+            presenter.CalculateAreaText();
+        }
+    }
+}
---
+++ MVPtest1/Presenters/RectanglePresenter.cs
@@ -0,0 +1,29 @@
+﻿using MVPtest1.Models;
+using MVPtest1.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPtest1.Presenters
+{
+    public class RectanglePresenter
+    {
+        IRectangle _rectangleView;
+
+        public RectanglePresenter(IRectangle view)
+        {
+            _rectangleView = view;
+        }
+
+        public void CalculateAreaText()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Length = double.Parse(_rectangleView.LengthText);
+            rectangle.Breadth = double.Parse(_rectangleView.BreadthText);
+
+            _rectangleView.AreaText = rectangle.CalculateArea().ToString();
+        }
+    }
+}
